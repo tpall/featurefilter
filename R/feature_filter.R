a4ad06469f18e7492cd45fd33de131027787c532
Taavi Päll <tapa741@gmail.com>
2018-02-18 21:03:14 +0200

    updates

--- R/feature_filter.R
+++ R/feature_filter.R
@@ -1,20 +1,26 @@
 
-# Filter out features with low counts
-# Uses raw sequence read counts and number of samples per group to filter 
-# out genes with low number of read counts in most samples.
+#' Filter out features with low counts
+#'
+#' Uses sequence read counts and number of samples per group to filter
+#' out genes with low number of read counts in most samples.
+#'
 #' @param rawcounts counts matrix
 #' @param n_samples minimum number of samples passing the filter
+#'
 #' @return logical vector
-#' @import edgeR
+#'
+#' @importFrom edgeR cpm
+#'
 #' @export
+#'
 feature_filter <- function(rawcounts, n_samples) {
-  
+
   # Calculate normalised counts
   cpms <- edgeR::cpm(rawcounts)
-  
+
   # Smallest libsize in millions of counts
   libsize <- min(colSums(rawcounts, na.rm = TRUE)) / 1e6
-  
+
   # Library size adjusted feature filter
   rowSums(cpms > 10 / libsize) >= n_samples
 }
